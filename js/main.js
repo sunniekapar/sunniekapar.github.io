0b63e6ca71432cae4a8d364cfce3af21f76bd837
Sunnie Kapar <kaparsunnie@gmail.com>
2023-06-12 17:54:43 -0400

    Commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
-const pdfEmbed = document.querySelector(".pdf-embed");
-const contentButtons = Array.from(document.querySelectorAll(".content-btn"));
-contentButtons.forEach(element => {
-    element.addEventListener("click", () => {
-        changePdf(element.id);
-    });
-});
+const navbar = document.querySelector(".navbar");
+let lastScrollPosition = 0;
+window.onscroll = () => {
+  if (scrollY === 0) {
+    navbar.classList.remove("nav-shadow");
+    navbar.classList.remove("nav-hide");
+  } else if (scrollY > lastScrollPosition) {
+    navbar.classList.remove("nav-shadow");
+    navbar.classList.add("nav-hide");
+  } else {
+    navbar.classList.add("nav-shadow");
+    navbar.classList.remove("nav-hide");
+  }
+  lastScrollPosition = scrollY;
+};
 
-function changePdf(pdfName) {
-    if(`pdfs/${pdfName}.pdf#page=1&zoom=95` != pdfEmbed.getAttribute("src"))
-        pdfEmbed.setAttribute("src", `pdfs/${pdfName}.pdf#page=1&zoom=95`);
-}
+const tablistButtons = Array.from(
+  document.querySelectorAll(".tab-list-button")
+);
+
+const tablistPannels = Array.from(document.querySelectorAll(".tab-list-body"));
+
+tablistButtons.forEach((button) => button.addEventListener("click", () => {
+  let currentPannel = tablistPannels.at(tablistButtons.indexOf(button));
+  if(currentPannel.classList.contains("hidden")) {
+    tablistButtons.forEach((b) => b.classList.remove("tab-visible"));
+    button.classList.add("tab-visible");
+    tablistPannels.forEach((pannel) => pannel.classList.add("hidden"));
+    currentPannel.classList.remove("hidden");
+  }
+}));
+
